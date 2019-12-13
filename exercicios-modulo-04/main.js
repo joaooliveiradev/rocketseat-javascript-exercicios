f9e51565ef2e6367924bc65bf0c0975d8a187de9
Joao Oliveira <joaooliveira1775@gmail.com>
2019-12-13 17:01:32 -0300

    fourth module done

---
+++ exercicios-modulo-04/main.js
@@ -0,0 +1,17 @@
+//Primeiro Exercicio
+function checaIdade(idade) {
+    return new Promise(function(resolve, reject){  
+        setTimeout(()=>{
+            return idade >= 18 ? resolve() : reject();
+        },2000)
+    });
+}
+   checaIdade(15)
+    .then(function() {
+    console.log("Maior que 18");
+    })
+    .catch(function() {
+    console.log("Menor que 18");
+    });
+
+   
